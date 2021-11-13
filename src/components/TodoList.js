8954ebf58e90db6bfa1c5c705ab422db291b1c94
KyungTae Bang <triple486@gmail.com>
2021-11-13 17:32:17 +0900

    add todo list section

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+  background: gray; /* 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
+`;
+
+function TodoList() {
+  return <TodoListBlock>TodoList</TodoListBlock>;
+}
+
+export default TodoList;
