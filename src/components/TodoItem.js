4853d9dc0bd60abcfab0486691d4f3cd10a87693
KyungTae Bang <triple486@gmail.com>
2021-11-16 11:00:55 +0900

    todo item - CheckCircle

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -26,3 +26,22 @@ const TodoItemBlock = styled.div`
     }
   }
 `;
+
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${props =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
