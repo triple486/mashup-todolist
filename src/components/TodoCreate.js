c208e60d9290c3a8049e18bfc9d87b655d7f3454
KyungTae Bang <triple486@gmail.com>
2021-11-22 23:57:12 +0900

    todo create - CircleButton - background

---
+++ src/components/TodoCreate.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { MdAdd } from 'react-icons/md';
+
+const CircleButton = styled.button`
+  background: #38d9a9;
+  &:hover {
+    background: #63e6be;
+  }
+  &:active {
+    background: #20c997;
+  }
+
+  
+`;
