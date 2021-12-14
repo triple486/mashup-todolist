d5a171c35847330ea7cab31315151b927db7c828
KyungTae Bang <triple486@gmail.com>
2021-12-14 14:35:05 +0900

    todo create - TodoCreate - InsertForm

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -80,5 +80,16 @@ function TodoCreate() {
 
   const onToggle = () => setOpen(!open);
 
-  
+  return (
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>
+            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      
+    </>
+  );
 }
