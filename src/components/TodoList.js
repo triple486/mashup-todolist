3357fb165db12f69db18c93ad9a52dd627ab2b6a
triple486 <triple486@gmail.com>
2022-07-18 14:00:31 +0900

    todos.map

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,10 +15,9 @@ function TodoList() {
 
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-      <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      {todos.map(todo => (
+        
+      ))}
     </TodoListBlock>
   );
 }
